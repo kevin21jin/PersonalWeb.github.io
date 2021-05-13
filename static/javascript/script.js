ceacb230bedc64963c6695487c76721447cc17db
Kevin Jin <83667515+kevin21jin@users.noreply.github.com>
2021-05-13 12:21:59 -0700

    Add a javascript file

---
+++ static/javascript/script.js
@@ -0,0 +1,59 @@
+let e1 = document.getElementById("target1");
+
+e1.onclick = changeColor1
+
+function changeColor1() {
+	let newColor = '';
+	var element = document.getElementById("target1");
+	element.style.color = "SteelBlue";
+}
+
+let e2 = document.getElementById("target2");
+
+e2.onclick = changeColor2
+
+function changeColor2() {
+	let newColor = '';
+	var element = document.getElementById("target2");
+	element.style.color = "SteelBlue";
+}
+
+let e3 = document.getElementById("target3");
+
+e3.onclick = changeColor3
+
+function changeColor3() {
+	let newColor = '';
+	var element = document.getElementById("target3");
+	element.style.color = "SteelBlue";
+}
+
+let e4 = document.getElementById("target4");
+
+e4.onclick = changeColor4
+
+function changeColor4() {
+	let newColor = '';
+	var element = document.getElementById("target4");
+	element.style.color = "SteelBlue";
+}
+
+let e5 = document.getElementById("target5");
+
+e5.onclick = changeColor5
+
+function changeColor5() {
+	let newColor = '';
+	var element = document.getElementById("target5");
+	element.style.color = "SteelBlue";
+}
+
+let e6 = document.getElementById("target6");
+
+e6.onclick = changeColor6
+
+function changeColor6() {
+	let newColor = '';
+	var element = document.getElementById("target6");
+	element.style.color = "SteelBlue";
+}
